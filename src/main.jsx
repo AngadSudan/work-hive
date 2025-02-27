5c89adcad6cd08c4432731d5e8607386c61dc0be
Angad Sudan <angadsudan453@gmail.com>
2025-02-27 10:16:48 +0530

    added routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,42 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import CompanyLogin from "./pages/CompanyLogin.jsx";
+import CompanySignup from "./pages/CompanySignup.jsx";
+import Home from "./pages/Home.jsx";
+import IndividualProjecs from "./pages/IndividualProjects.jsx";
+import Landing from "./pages/Landing.jsx";
+import Login from "./pages/Login.jsx";
+import Organization from "./pages/Organization.jsx";
+import Project from "./pages/Project.jsx";
+import Task from "./pages/Task.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/home" element={<App />}>
+        <Route index path="" element={<Home />} />
+        <Route index path="organization" element={<Organization />} />
+        <Route index path="projects" element={<Project />} />
+        <Route index path="projects/:id" element={<IndividualProjecs />} />
+        <Route index path="task" element={<Task />} />
+      </Route>
+      <Route index path="" element={<Landing />} />
+      <Route index path="/login" element={<Login />} />
+      <Route index path="/company-login" element={<CompanyLogin />} />
+      <Route index path="/company-signup" element={<CompanySignup />} />
+    </>
+  )
+);
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
